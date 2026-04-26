4e63c10c1b97cc0f37bd194c08433a7459adbb97
Younes Mouafak <ymouafak@e3r4p11.1337.ma>
2026-04-26 18:25:07 +0100

    toma 9ala mada threads

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ymouafak <ymouafak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 14:14:58 by ymouafak          #+#    #+#             */
-/*   Updated: 2026/04/26 15:24:09 by ymouafak         ###   ########.fr       */
+/*   Updated: 2026/04/26 16:25:46 by ymouafak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,64 @@ void *test_func(void *ptr)
 	return (NULL);
 }
 
+void ft_clean(t_arguments *args, t_coder *coders, t_dongle *dongles, pthread_t *ids, pthread_mutex_t *lock_in)
+{
+	int i;
+
+	i = 0;
+    pthread_mutex_destroy(lock_in);
+    free(coders);
+	if (dongles)
+	{
+		while (i < args->num_coders)
+		{
+			pthread_mutex_destroy(&dongles[i].lock);
+			i++;
+		}
+		free(dongles);
+	}
+    free(args);
+    free(ids);
+}
+
+void innit_coders(t_arguments *args, t_coder *coders, t_dongle *dongles, struct timeval start, pthread_mutex_t *lock_in)
+{
+	int	i;
+
+	i = 0;
+	while (i < args->num_coders)
+	{
+		pthread_mutex_init(&dongles[i].lock, NULL);
+		dongles[i].id = i;
+		dongles[i].is_available = 1;
+		coders[i].id = i + 1;
+		coders[i].left = &dongles[(i - 1 + args->num_coders) % args->num_coders];
+		coders[i].right = &dongles[i];
+		coders[i].start_time = start;
+		coders[i].lock_in = lock_in;
+		coders[i].args = args;
+		i++;
+	}
+}
+
+void launch_threads(pthread_t *ids, t_coder *coders, int num_coders)
+{
+    int i;
+
+    i = 0;
+    while (i < num_coders)
+    {
+        pthread_create(&ids[i], NULL, test_func, &coders[i]);
+        i++;
+    }
+    i = 0;
+    while (i < num_coders)
+    {
+        pthread_join(ids[i], NULL);
+        i++;
+    }
+}
+
 int	main(int argc, char **str)
 {
 	t_arguments	*args;
@@ -57,7 +115,6 @@ int	main(int argc, char **str)
 	t_dongle *dongles;
 	struct timeval start;
 	pthread_t *ids;
-	int i;
 	pthread_mutex_t lock_in;
 
 	if (argc != 9)
@@ -68,59 +125,28 @@ int	main(int argc, char **str)
 	args = parsing(str + 1);
 	if (!args)
 		return(1);
-	i = 0;
+	pthread_mutex_init(&lock_in, NULL);
 	coders = malloc(args->num_coders * sizeof(t_coder));
 	if (!coders)
 	{
-		free(args);
+		ft_clean(args, NULL, NULL, NULL, &lock_in);
 		return(1);
 	}
 	dongles = malloc(args->num_coders * sizeof(t_dongle));
 	if (!dongles)
 	{
-		free(coders);
-		free(args);
+		ft_clean(args, coders, NULL, NULL, &lock_in);
 		return(1);
 	}
 	gettimeofday(&start, NULL);
-	pthread_mutex_init(&lock_in, NULL);
-	while (i < args->num_coders)
-	{
-		pthread_mutex_init(&dongles[i].lock, NULL);
-		dongles[i].id = i;
-		dongles[i].is_available = 1;
-		coders[i].id = i + 1;
-		coders[i].left = &dongles[(i - 1 + args->num_coders) % args->num_coders];
-		coders[i].right = &dongles[i];
-		coders[i].start_time = start;
-		coders[i].lock_in = &lock_in;
-		coders[i].args = args;
-		i++;
-	}
+	innit_coders(args, coders, dongles, start, &lock_in);
 	ids = malloc(args->num_coders * sizeof(pthread_t));
 	if (!ids)
 	{
-		free(dongles);
-		free(coders);
-		free(args);
+		ft_clean(args, coders, dongles, NULL, &lock_in);
 		return(1);
 	}
-	i = 0;
-	while (i < args->num_coders)
-	{
-		pthread_create(&ids[i], NULL, test_func, &coders[i]);
-		i++;
-	}
-	i = 0;
-		i = 0;
-	while (i < args->num_coders)
-	{
-		pthread_join(ids[i], NULL);
-		i++;
-	}
-    free(args);
-    free(coders);
-    free(dongles);
-	pthread_mutex_destroy(&lock_in);
+	launch_threads(ids, coders, args->num_coders);
+	ft_clean(args, coders, dongles, ids, &lock_in);
 	return (0);
 }
